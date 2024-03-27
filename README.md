1e214f3113a6ad65367a9cfb3eec0c588247f89d
feberts <64541491+feberts@users.noreply.github.com>
2024-03-27 19:36:47 +0100

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Markup stripper
+
+I use this tool to remove markup elements such as text decorations and macros from LaTeX and Markdown text for the purpose of more reliable spellchecking. Spell checkers are sometimes confused by too much markup; latex macros can even hide some grammar errors, that the spell checker would otherwise recognize.
+
+This is **not** a universal tool. I built this mainly to remove my own latex macros, and I only put it here to have it hosted by github pages. If you find this useful anyway, then you need to add new entries to the replacement functions.
---
+++ strip.js
@@ -0,0 +1,129 @@
+var generic_replacement = 'Kuchen';
+
+const textarea = document.getElementsByName('textarea')[0];
+var text;
+
+function replace(pattern, replacement)
+{
+    text = text.replaceAll(pattern, replacement);
+}
+
+function stripLaTeX()
+{
+    text = textarea.value;
+    let text_prev = '';
+
+    do // replace nested macros inside out
+    {
+        console.log('strip latex');
+
+        text_prev = text;
+
+        // direct replacements:
+        replace('"=', '-'); // "=
+        replace('--', '–'); // --
+        replace('~', ' '); // ~
+        replace('\\,', ' '); // \,
+        replace('"-', ''); // "-
+        replace(/\\noindent */g, ''); // \noindent
+        replace(/ *\\item */g, ''); // \item
+        replace('{}', ''); // {}
+        replace('\\ ', ' '); // \ plus space
+
+        // discard optional macro arguments:
+        replace(/\[[^\[\]]+\]/g, ''); // []
+
+        // discard surrounding macro, keep content:
+        replace(/\\mbox{([^{}]+)}/g, '$1'); // \mbox{}
+        replace(/\\emph{([^{}]+)}/g, '$1'); // \emph{}
+        replace(/\\textit{([^{}]+)}/g, '$1'); // \textit{}
+        replace(/\\textbf{([^{}]+)}/g, '$1'); // \textbf{}
+        replace(/\\texttt{([^{}]+)}/g, '$1'); // \texttt{}
+
+        // discard surrounding macro and content:
+        replace(/ *\\cite{[^{}]+}/g, ''); // \cite{} \cite[]{} \cite[][]{}
+        replace(/\\(begin|end){[^{}]+}/g, ''); // \begin{} \end{}
+
+        // replace surrounding macro, keep content:
+        replace(/ *\\footnote{([^{}]+)}/g, ' ($1)'); // \footnote{}
+
+        // replace surrounding macro and content:
+        replace(/\\ic{[^{}]+}/g, generic_replacement); // \ic{} \ic[]{}
+        replace(/\\ref{[^{}]+}/g, '42'); // \ref{}
+
+        // various replacements:
+        replace('\\LaTeX', generic_replacement);
+        replace('\\TeX', generic_replacement);
+        replace('WebAssembly', generic_replacement);
+        replace('Actions', generic_replacement);
+        replace('Emscripten', generic_replacement);
+        replace('GitHub-Pages', generic_replacement);
+        replace('\\swift', generic_replacement);
+        replace('\\pdf', generic_replacement);
+        replace('\\live', generic_replacement);
+    }
+    while(text != text_prev)
+
+    textarea.value = text;
+}
+
+/*
+test cases:
+
+abc"=abc
+abc -- abc
+abc~abc
+abc\,abc
+abc"-abc
+\noindent abc
+  \item abc
+abc{} abc
+abc\ abc
+
+abc \mbox{xyz} abc
+abc \emph{xyz} abc
+abc \textit{xyz} abc
+abc \textbf{xyz} abc
+abc \texttt{xyz} abc
+
+abc \cite{xyz} abc
+abc \cite[xyz]{xyz} abc
+abc \cite[xyz][xyz]{xyz} abc
+\begin{abc}
+\begin{abc}[abc]
+\end{abc}
+
+abc\footnote{xyz} abc
+
+abc \ic{xyz} abc
+abc \ic[xyz]{xyz} abc
+abc \ref{xyz} abc
+
+abc \emph{xyz \textbf{mno} xyz} abc
+abc \textbf{xyz \emph{mno} xyz} abc
+abc \textbf{xyz \emph{mno} \texttt{pqr} xyz} abc
+abc \textbf{xyz \mbox{\emph{mno} \texttt{pqr}} xyz} abc
+abc \textbf{xyz \emph{mno} \ic[xyz]{pqr} xyz} abc
+*/
+
+function stripMarkdown()
+{
+    console.log('strip markdown');
+
+    text = textarea.value;
+
+    replace(/[_*]/g, ''); // text decoration
+    replace(/`([^`]+)`/g, generic_replacement); // inline code
+    replace(/\[([^\]]+)\]\([^)]+\)/g, '$1'); // link
+
+    textarea.value = text;
+}
+
+/*
+test cases:
+
+abc *xyz* abc _xyz_ abc
+abc **xyz** abc __xyz__ abc
+abc [xyz](https://de.wikipedia.org/) abc
+abc `xyz` abc `xyz` abc
+*/
